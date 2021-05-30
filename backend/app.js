9f50c65d0f2ae89af8c9f2ad36bb9d11fc6573ae
phancongtaidhsp <phancongtaicntt2@gmail.com>
2021-05-30 17:06:25 +0700

    testing

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,24 @@
 var robot = require("robotjs");
-
+const delay = require('delay');
 setTimeout(function()
 {
-  robot.moveMouse(920, 370);
-  robot.mouseClick();
-  robot.typeString("t");
-  robot.moveMouse(1200, 734);
-  robot.mouseClick();
-  robot.typeStringDelayed(`git add . ; git commit -m "testing" ; git push`,0.01);
+  setInterval(function() {
+    (async() => {
+      robot.moveMouse(920, 370);
+      robot.mouseClick();
+      robot.typeString("t");
+  
+      robot.moveMouse(370, 55);
+      robot.mouseClick();
+      robot.moveMouse(370, 270);
+      await delay(200);
+      robot.mouseClick();
+  
+      robot.moveMouse(1200, 734);
+      robot.mouseClick();
+      robot.keyTap('up');
+      robot.keyTap('enter');
+    })()
+  },5000)
 
-}, 3000);
+}, 8000);
