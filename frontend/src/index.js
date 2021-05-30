23ef42cf94ccf97785c98fc2568b14fa2664a48a
phancongtaidhsp <phancongtaicntt2@gmail.com>
2021-05-30 16:17:53 +0700

    Testing one line command

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("roo")
+  document.getElementById("rooot")
 );
 
 // If you want your app to work offline and load faster, you can change
