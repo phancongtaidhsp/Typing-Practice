5cb41627733e5bc428d7aa76301d05f9964831db
phancongtaidhsp <phancongtaicntt2@gmail.com>
2021-05-30 16:48:43 +0700

    testing

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,12 @@ var robot = require("robotjs");
 
 // setTimeout(function()
 // {
-  robot.moveMouse(1200, 750);
+  robot.moveMouse(920, 370);
+  robot.mouseClick();
+  robot.typeString("t");
+  robot.keyToggle('s','down','control')
+  robot.moveMouse(1200, 734);
   robot.mouseClick();
   robot.typeString(`git add . ; git commit -m "testing" ; git push`);
-  robot.keyTap('enter');
-  // robot.moveMouse(920, 370);
-  // robot.mouseClick();
-  // robot.typeString("t");
-  // robot.moveMouse(1200, 734);
-  // robot.mouseClick();
-  // robot.typeString(`git add . ; git commit -m "testing" ; git push`);
 
 // }, 5000);
